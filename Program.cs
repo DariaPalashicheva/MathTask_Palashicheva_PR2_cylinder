69895f3e3d405e7204898c87235bac8750dbeb60
u31504 <u31504@P31504>
2022-11-19 09:27:20 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTask_Palashicheva_PR2_cylinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Geometry
+    {
+        public double RectangleArea(double r, double h)
+        {
+            return Math.PI * r * r * h;
+        }
+    }
+
+}
